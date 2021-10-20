abd04efd76f2bc9e075b1626c682690581adff50
Kiberio <dhiroshi3@gmail.com>
2021-10-20 14:38:44 -0300

    Comentarios

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -16,20 +16,24 @@ namespace ConsoleApp7
             list.Insert(2, "Lucas");
 
 
-                //Qntd de elementos da lista q começam com a
+                //Qntd de elementos da lista possuem a letra "a"
                 Console.WriteLine("Qnt de elementos na Lista: " + list.Count);
                 string s1 = list.Find(x => x[0] == 'A');
+                //Primeiro elementos da lista começam com "A"
                 Console.WriteLine("Primeiro elemento que começa com a letra A: " + s1);
                 string s2 = list.FindLast(x => x[0] == 'A');
+                //Ultimo elemento que começa com "A"
                 Console.WriteLine("Ultimo elemento que começa com a letra A: " + s2);
-          
+            //Encontrar palavras que começam com "a" e depois q terminam com "a"
             int pos1 = list.FindIndex(x => x[0] == 'A');
             Console.WriteLine("Primeira Posição de 'A': " + pos1);
             int pos2 = list.FindLastIndex(x => x[0] == 'A');
             Console.WriteLine("Última Posição de 'A': " + pos2);
 
+            //Encontrar palavras com 5 letras
             List<string> list2 = list.FindAll(x => x.Length == 5);
             Console.WriteLine("-------------------------------");
+            //Remover "Andre" da lista
             foreach (string obj in list2)
             {
                 Console.WriteLine(obj);
@@ -37,6 +41,7 @@ namespace ConsoleApp7
                 list.Remove("Andre");
                
             }
+            //Imprimir a lista com as modificações
             Console.WriteLine("===================");
             foreach (string obj in list)
             {
@@ -45,22 +50,26 @@ namespace ConsoleApp7
 
             list.Remove("Andre");
             Console.WriteLine("===================");
+            //Imprimir a lista novamente
             foreach (string obj in list)
             {
                 Console.WriteLine(obj);
             }
+            //Remover palavras q possuam "w" e imprimir a lista atualizada
             list.RemoveAll(x => x[0] == 'W');
             Console.WriteLine("+++++++++++++++++++");
             foreach (string obj in list)
             {
                 Console.WriteLine(obj);
             }
+            //Removes a palavra no "3" lugar e atualizar a lista
             list.RemoveAt(3);
             Console.WriteLine("xxxxxxxxxxxxxxxxxxxxxx");
             foreach (string obj in list) 
             {
                 Console.WriteLine(obj); 
             }
+            //Remover palavras com 1 e 2 letras e imprimir lista novamente
             list.RemoveRange(1, 2);
             Console.WriteLine("xxxxxxxxxxxxxxxxxxxxxx");
             foreach (string obj in list) 
